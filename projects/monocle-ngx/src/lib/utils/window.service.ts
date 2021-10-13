6cde4e32d1a3b236c98dd335b2334072663bea36
Michael Parkins <mparkin1@progressive.com>
2021-10-13 15:07:07 -0400

    Lint refactor on window.service.ts

--- projects/monocle-ngx/src/lib/utils/window.service.ts
+++ projects/monocle-ngx/src/lib/utils/window.service.ts
@@ -2,28 +2,29 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class WindowService {
+  constructor(private window: Window) {}
 
-  addEventListener = this.nativeWindow.addEventListener.bind(this.nativeWindow);
-  removeEventListener = this.nativeWindow.removeEventListener.bind(this.nativeWindow);
+  addEventListener = this.window.addEventListener.bind(this.nativeWindow);
+  removeEventListener = this.window.removeEventListener.bind(this.nativeWindow);
 
   get windowSize(): string {
-    return this.nativeWindow.innerWidth + 'x' + this.nativeWindow.innerHeight;
+    return `${this.window.innerWidth}x${this.window.innerHeight}`;
   }
 
   get screenResolution(): string {
-    return this.nativeWindow.screen.width + 'x' + this.nativeWindow.screen.height;
+    return `${this.window.screen.width}x${this.window.screen.height}`;
   }
 
   get hostName(): string {
-    return this.nativeWindow.location.hostname;
+    return this.window.location.hostname;
   }
 
   get url(): string {
-    return this.nativeWindow.location.href;
+    return this.window.location.href;
   }
 
-  get nativeWindow() {
-    return (window as any);
+  // FIXME: this is be used in google-tage-manager.service.ts incorrectly and needs to be appropriately typed as Window or removed
+  get nativeWindow(): any {
+    return this.window;
   }
-
 }
