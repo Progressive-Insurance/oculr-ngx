b073bcffface87798ce72dbccedc11cfefb8be35
Michael Parkins <michael.e.parkins@hotmail.com>
2024-09-26 15:05:46 +0000

    ng 18

--- projects/oculr-ngx/src/lib/oculr-ngx.module.ts
+++ projects/oculr-ngx/src/lib/oculr-ngx.module.ts
@@ -7,7 +7,7 @@
  */
 
 import { CommonModule } from '@angular/common';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { APP_INITIALIZER, ModuleWithProviders, NgModule } from '@angular/core';
 import { ConsoleService } from './destinations/console/console.service';
 import { HttpApiService } from './destinations/http-api/http-api.service';
@@ -26,12 +26,8 @@ import { InitializationService } from './services/initialization.service';
 import { LocationService } from './services/location.service';
 import { TimeService } from './services/time.service';
 
-@NgModule({
-  imports: [CommonModule, HttpClientModule],
-  declarations: [DisplayDirective, ClickDirective, ChangeDirective, FocusDirective, TrackValidationDirective],
-  exports: [DisplayDirective, ClickDirective, ChangeDirective, FocusDirective, TrackValidationDirective],
-  providers: [],
-})
+@NgModule({ declarations: [DisplayDirective, ClickDirective, ChangeDirective, FocusDirective, TrackValidationDirective],
+    exports: [DisplayDirective, ClickDirective, ChangeDirective, FocusDirective, TrackValidationDirective], imports: [CommonModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class OculrAngularModule {
   static forRoot(): ModuleWithProviders<OculrAngularModule> {
     return {
